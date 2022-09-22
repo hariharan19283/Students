394af92ff88e9d3c273b6078cd81aa8d7df21114
hariharan19283 <113096845+hariharan19283@users.noreply.github.com>
2022-09-22 02:43:14 -0700

    Add files via upload

---
+++ Downward.java
@@ -0,0 +1,15 @@
+public class Downward  
+{  
+public static void main(String[] args)  
+{  
+int rows=4;      
+for (int i= rows-1; i>=0 ; i--)  
+{  
+for (int j=0; j<=i; j++)  
+{  
+System.out.print("*" + " ");  
+}  
+System.out.println();  
+}  
+}  
+}
