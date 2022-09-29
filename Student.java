87df2bafb2d6b4bd23ffec139cc5833b81ccb80e
hariharan19283 <113096845+hariharan19283@users.noreply.github.com>
2022-09-29 01:14:53 -0700

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,25 +1,59 @@
-Hari haran
-CG2576
 import java.util.Scanner;
 
-class Student
+public class Student
 {
-   
-  public static void main(String args[])
-  {
-
     String name;
-     int id,Attendence;
+    int id;
+    int m1;
+    int m2;
+    int m3;
+    
 
-     Scanner SC = new Scanner(System.in);
+    public Student(String n, int i, int s1, 
+    int s2, int s3) {
+        name = n;
+        id = i;
+        m1 = s1;
+        m2 = s2;
+        m3 = s3;
+    }
 
-    System.out.print("Enter name: ");
-    name= SC.nextLine();
-    System.out.print("Enter student id: ");
-    id= SC.nextInt();
-    System.out.print("Enter Attendence: ");
-    Attendence=SC.nextInt();
+    public Student() {
+        name = "";
+        id = 0;
+        m1 = 0;
+        m2 = 0;
+        m3 = 0;
+      
+    }
 
-   }
-  }
+    public void accept() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        name = in.nextLine();
+        System.out.print("Enter id: ");
+        id = in.nextInt();
+        System.out.print("Enter Subject 1 Marks: ");
+        m1 = in.nextInt();
+        System.out.print("Enter Subject 2 Marks: ");
+        m2 = in.nextInt();
+        System.out.print("Enter Subject 3 Marks: ");
+        m3 = in.nextInt();
+    }
 
+   
+
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("id: " + id);
+        System.out.println("Subject 1 Marks: " + m1);
+        System.out.println("Subject 2 Marks: " + m2);
+        System.out.println("Subject 3 Marks: " + m3);
+      }
+
+    public static void main(String args[]) {
+        Student obj = new Student();
+        obj.accept();
+        obj.display();
+    }
+}
